c0604779e098d5a0660871a25163d950b9accbf6
Tiffany <chin.tiffany108@gmail.com>
2020-09-27 00:05:50 -0400

    add tab keypress to focus on color

--- src/Typeahead.jsx
+++ src/Typeahead.jsx
@@ -5,7 +5,31 @@ class Typeahead extends React.Component {
     super(props);   // why is super crossed out? deprecated API?
     this.state = {
       input: '',
+      backgroundColor: 'white',
     }
+
+    this.list = [];
+  }
+
+  componentDidMount() {
+    let that = this;
+
+    document.addEventListener('keydown', function(e) {
+      if (e.key === 'Enter') {
+        let liColors = document.getElementsByClassName('each-color');
+        for (let i = 0; i < liColors.length; i++) {
+          let li = liColors[i];
+          if (document.activeElement === li) {
+            that.setState({ backgroundColor: li.getAttribute('id')});
+          }
+        }
+      } else if (e.key === 'Tab') {
+        if (document.activeElement === document.getElementsByTagName('input')[0]) {
+          let liColors = document.getElementsByClassName('each-color');
+          liColors[0].focus();
+        }
+      }
+    });
   }
 
   update(field) {
@@ -16,33 +40,51 @@ class Typeahead extends React.Component {
 
   render() {
     const { list } = this.props;
-    const { input } = this.state;
+    const { input, backgroundColor } = this.state;
 
-    let colorsList = null;
+    let colorsList = null, errorMsg = null;
     if (input.length > 0 && input.split("").some(char => char !== ' ')) {
       let search = input.toLowerCase();
       while (search.startsWith(' ')) {
         search = search.slice(1);
       }
 
-      colorsList = list.filter(l => (l.toLowerCase()).startsWith(search)).map((l, i) => {
+      this.list = list.filter(l => (l.toLowerCase()).startsWith(search));
+      colorsList = this.list.map((l, i) => {
+        let bold = l.slice(0, search.length);
+        let normal = l.slice(search.length);
         return (
-          <li key={i}>{l}</li>
+          <li key={i} className='each-color' tabIndex='1' id={l.toLowerCase()}>
+            <span className='bold'>{bold}</span>
+            <span className='normal'>{normal}</span>
+          </li>
         )
       })
+
+      if (!colorsList.length) {
+        errorMsg = (
+          <span className='error'>Oops! Looks like there are no colors that start with those letters. Please try again. </span>
+        )
+      }
     }
 
     return (
-      <section className='container'>
-        <div className='main'>
-          <span className='title'>Welcome to Color Finder!</span>
-          <input type="text" 
-            placeholder="Begin typing for colors to populate below..."
-            value={input} 
-            onChange={this.update('input')}/>
-          <ul>
-            {colorsList}
-          </ul>
+      <section className='background' style={{ backgroundColor: backgroundColor }}>
+        <div className='container'>
+          <div className='main'>
+            <span className='title'>Welcome to Color Finder!</span>
+            <input type="text" 
+              placeholder="Begin typing to find a color..."
+              value={input} 
+              className={colorsList && colorsList.length ? 'show-list' : ''}
+              onChange={this.update('input')}/>
+            {colorsList ? 
+              <ul className='colors-list'>
+                {colorsList}
+              </ul> : null
+            }
+            {errorMsg}
+          </div>
         </div>
       </section>
     )
